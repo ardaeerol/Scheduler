be4939ce7c2b2693e09816bf12547a8d8bf176f5
Arda Erol <84896378+ardaeerol@users.noreply.github.com>
2022-11-01 21:25:35 +0300

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,23 +1,23 @@
-public class Surucu {
+public class Driver {
     public static void main(String[] args) {
     job j0 = new job(0,0,"H",3);
     job j1 = new job(0,1,"H",2);
     job j2 = new job(0,2,"M",3);
     job j3 = new job(3,3,"H",3);
 
-    Scheduler programlayici = new Scheduler();
-    programlayici.setResourcesCount(2);
-    programlayici.add(j0);
-    programlayici.add(j1);
-    programlayici.add(j2);
-    programlayici.add(j3);
-    programlayici.run();
+    Scheduler programmer = new Scheduler();
+    programmer.setResourcesCount(2);
+    programmer.add(j0);
+    programmer.add(j1);
+    programmer.add(j2);
+    programmer.add(j3);
+    programmer.run();
     System.out.println("-------");
-    programlayici.utilization(1); // kaynak 1, toplam 6 birim çalışmış ve arada boş kaldığı zaman              // olmamış, 6/6 = 1
+    programmer.utilization(1); 
     System.out.println("-------");
-    programlayici.resourceExplorer(2);
+    programmer.resourceExplorer(2);
     System.out.println("-------");
-    programlayici.jobExplorer(j3);
+    programmer.jobExplorer(j3);
 
 
 
